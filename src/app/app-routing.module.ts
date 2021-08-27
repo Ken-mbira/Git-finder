d15a91d7d221316ce00d76509f406df2480983d4
ken <ken.mbira@student.moringaschool.com>
2021-08-27 10:18:17 +0300

    add a router link to the search page from the landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent},
+  { path: 'search', component: SearchPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
