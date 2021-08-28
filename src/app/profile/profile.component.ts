24a0fb57603331bd6c06110db4f84e7501e6cb59
ken <ken.mbira@student.moringaschool.com>
2021-08-28 17:14:56 +0300

    made the findings view toggle between the followers following and repos

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,6 +9,27 @@ import { ProfileServiceService } from './../profile-http/profile-service.service
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+
+  profile : User[] = [
+    new User("Ken Mbira", "blah blah blah", 1, 2, 1,"sdfadfasf")
+  ];
+
+  showRepos(index){
+    this.profile[index].showRepo = true;
+    this.profile[index].showFollowers= false;
+    this.profile[index].showFollowing = false;
+  }
+  showFollowers(index){
+    this.profile[index].showRepo = false;
+    this.profile[index].showFollowers= true;
+    this.profile[index].showFollowing = false;
+  }
+  showFollowing(index){
+    this.profile[index].showRepo = false;
+    this.profile[index].showFollowers= false;
+    this.profile[index].showFollowing = true;
+  }
+
   constructor(){
   }
 
