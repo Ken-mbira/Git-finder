3d777db5118a338edf3112787a8faef57fcbb40f
ken <ken.mbira@student.moringaschool.com>
2021-08-31 12:42:37 +0300

    add the client service and id to the environments.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiKey: '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'
+  clientSecret: "097e329d366a8782ec851426a81c376b29292ab9",
+  clientId: "515d98aa8fa37ee53e7b"
 };
