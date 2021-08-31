ffb3f22235b7a5dc2ae3c7072f1d73b3e8f567c5
ken <ken.mbira@student.moringaschool.com>
2021-08-31 10:02:57 +0300

    add new key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  apiKey: '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'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  apiKey: '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'
 };
 
 /*
